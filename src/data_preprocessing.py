db5cf0d8612dc00283de85b3a6dd0be3aaa084ae
Biljana Rankovic Plazinic <beexy86@gmail.com>
2026-08-20 00:29:50 +0200

    data_preprocessing.py added

---
+++ src/data_preprocessing.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder 
+
+#Definisanje ciljne promenljive
+TARGET_COLUMN = "price_usd"
+
+#Definisanje numeričkih karakteristika
+NUMERIC_FEATURES = ['year', 'mileage_kilometers', 'volume_cm3']
+
+#Definisanje kategorijskih karakteristika
+CATEGORICAL_FEATURES = ['make', 'model', 'condition', 'fuel_type', 'color', 'transmission', 'drive_unit', 'segment']
+
+#Pomoćna funkcija vraća sve ulazne kolone koje će model koristiti:
+
+def get_all_feature_columns() -> list[str]:
+    return NUMERIC_FEATURES + CATEGORICAL_FEATURES 
+
+#Razdvajanje ulaznih karakteristika i ciljne promenljive
+
+def split_features_and_target(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
+ 
+    X = df[get_all_feature_columns()].copy()
+    y = df[TARGET_COLUMN].copy()
+ 
+    return X, y
+
+#Pretprocesiranje numeričkih kolona
+def _build_numeric_transformer() -> Pipeline:
+ 
+    numeric_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(missing_values=pd.NA, strategy="median")),
+            ("scaler", StandardScaler()),
+        ]
+    )
+ 
+    return numeric_transformer
+
+#Pretprocesiranje nominalnih kategorijskih kolona
+def _build_categorical_transformer() -> Pipeline:
+ 
+    categorical_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(missing_values=pd.NA, strategy="most_frequent")),
+            ("encoder", OneHotEncoder(handle_unknown="ignore", sparse_output=False)),
+        ]
+    )
+ 
+    return categorical_transformer
+
+def build_preprocessor() -> ColumnTransformer:
+ 
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", _build_numeric_transformer(), NUMERIC_FEATURES),
+            ("cat", _build_categorical_transformer(), CATEGORICAL_FEATURES),
+        ],
+        remainder="drop"
+    )
+ 
+    return preprocessor
