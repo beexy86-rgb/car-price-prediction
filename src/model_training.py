87532fbda49e24bf71673e2945ab45db78f6ed9a
Biljana Rankovic Plazinic <beexy86@gmail.com>
2026-08-20 00:38:54 +0200

    korekcije

---
+++ src/model_training.py
@@ -0,0 +1,50 @@
+import joblib
+import pandas as pd
+ 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+ 
+from data_preprocessing import (
+    split_features_and_target,
+    build_preprocessor
+)
+
+DATA_PATH = "data/car_data_cleaned_with_features.csv"
+MODEL_PATH = "models/linear_regression_model.joblib"
+
+print("Loading dataset...")
+ 
+df = pd.read_csv(DATA_PATH)
+
+print("Splitting features and target...")
+ 
+X, y = split_features_and_target(df)
+
+print("Splitting data into training and test sets...")
+ 
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+print("Creating model pipeline...")
+ 
+model = Pipeline(
+    steps=[
+        ("preprocessor", build_preprocessor()),
+        ("regressor", LinearRegression()),
+    ]
+)
+
+print("Training model...")
+ 
+model.fit(X_train, y_train)
+
+print("Saving model...")
+ 
+joblib.dump(model, MODEL_PATH)
+ 
+print(f"Model saved to: {MODEL_PATH}")
